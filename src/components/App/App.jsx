b853b50fa794504a4180ea7824edcf78d3a26a88
avapeksha-prog <avapeksha@gmail.com>
2026-08-20 16:49:46 +0530

    tryHero

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Project from "./components/Project";
@@ -9,6 +8,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 import "./App.css";
+import Hero from "../Hero/Hero";
 
 function App() {
 
